7a585b9a4169704dd531647227d92e4ec463c984
ChristianArchuleta <christianarchuleta@tamu.edu>
2020-08-06 10:00:20 -0500

    Updates

--- precache-manifest.1300c5d8b11f04147cf92159c402479b.js
+++ precache-manifest.1300c5d8b11f04147cf92159c402479b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ecab16bba6cc820457c03ee092e225ab",
+    "revision": "3f35fa11c8aa67293ddc822e7b90ddb2",
     "url": "/DominoCounter/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/DominoCounter/static/css/2.0939e70c.chunk.css"
   },
   {
-    "revision": "fbfaff684c0aa9f1100d",
+    "revision": "05b03899a4ebe63dd699",
     "url": "/DominoCounter/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/DominoCounter/static/js/2.fba4fa59.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fbfaff684c0aa9f1100d",
-    "url": "/DominoCounter/static/js/main.d0595cf2.chunk.js"
+    "revision": "05b03899a4ebe63dd699",
+    "url": "/DominoCounter/static/js/main.e518c1f7.chunk.js"
   },
   {
     "revision": "a216e8e1998058261835",
